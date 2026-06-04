7b84eaf94c5457319566eda955d3b68fe937c9c9
vianapopatiya004 <vianapopatiya004@gmail.com>
2026-06-04 17:53:29 +0530

    Homework - Raj's class

---
+++ operatorprecedence/nestedconditionalstatements/homework1.py
@@ -0,0 +1,9 @@
+age = int(input("Enter the student's age: "))
+
+if age >= 10:
+    if age <= 20:
+        print("Student can enroll in the class.")
+    else:
+        print("Student cannot enroll in the class.")
+else:
+    print("Student cannot enroll in the class.")
