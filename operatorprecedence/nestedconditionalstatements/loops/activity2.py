8416a6d44f93cb73c95c32d3a6fe0ca2093bc984
vianapopatiya004 <vianapopatiya004@gmail.com>
2026-06-06 17:07:27 +0530

    loops- for loo[

---
+++ operatorprecedence/nestedconditionalstatements/loops/activity2.py
@@ -0,0 +1,10 @@
+string = input("Please enter your own String:")
+
+string2 = ('')
+for i in string:
+    string2 =  i + string2 
+    print(string2)
+
+print("\nThe Orignal String=", string)
+print("The Reversed String=", string2)
+
