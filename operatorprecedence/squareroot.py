915fc3f99de593bf004bb06f5fc0f8d6ddfac374
vianapopatiya004 <vianapopatiya004@gmail.com>
2026-06-06 23:51:47 +0530

    square roots

---
+++ operatorprecedence/squareroot.py
@@ -0,0 +1,7 @@
+num = float(input("Enter a number: "))
+
+if num >= 0:
+    root = num ** 0.5
+    print("Square Root =", root)
+else:
+    print("Square root of a negative number cannot be found.")
