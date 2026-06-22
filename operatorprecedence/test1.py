4cd182480bb8ef7df827dd292d4fc40cdfaedf26
vianapopatiya004 <vianapopatiya004@gmail.com>
2026-06-22 16:38:05 +0530

    final test

--- operatorprecedence/test1.py
+++ operatorprecedence/test1.py
@@ -2,21 +2,3 @@ secret = 12
 lives = 5
 
 
-while lives > 0:
-    guess = int(input("Guess the number"))
-
-    if guess == secret:
-         print("You Win")
-        
-    difference = guess - secret 
-    if difference < 0:
-         difference = -difference
-    if difference <= 4:
-        print("Ice cold")
-    elif difference  <= 6:
-        print("cold")
-    elif difference  <= 10:
-        print("Warm")
-    else: 
-        print("Hot")
-
