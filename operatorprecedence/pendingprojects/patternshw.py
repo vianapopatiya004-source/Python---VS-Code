df9278aac5bdec1b293c107e8d6161d9c65718b6
vianapopatiya004 <vianapopatiya004@gmail.com>
2026-06-22 16:52:48 +0530

    changes for pattern

--- operatorprecedence/pendingprojects/patternshw.py
+++ operatorprecedence/pendingprojects/patternshw.py
@@ -2,15 +2,12 @@ rows = int(input("Enter the number of rows: "))
 
 for i in range(1, rows + 1):
 
-    # Left triangle
     for j in range(i):
         print("*", end=" ")
 
-    # Gap between triangles
     for j in range(2 * (rows - i)):
         print(" ", end=" ")
 
-    # Right triangle
     for j in range(i):
         print("*", end=" ")
 
